f711aac11a0c236c8fc712b32d6397ca0c4b4548
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-22 21:16:46 +0300

    refactor

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # wpf-starter
 
 Simple WPF starter project.
-
-## Resources
-
-* [Segoe MDL2 Assets icons](https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-ui-symbol-font)
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -8,16 +8,6 @@ namespace WPFStarter {
     public MainWindow() {
       InitializeComponent();
     }
-
-    private void OnSizeChanged(object sender, SizeChangedEventArgs args) {
-      // @NOTE: This hack is here because:
-      // Given: AllowsTransparency="True"
-      // When: Window maximized
-      // Then: Window size is greater than the monitor size... WTF?
-
-      var thickness = (this.WindowState == WindowState.Maximized ? 8 : 0);
-      this.BorderThickness = new System.Windows.Thickness(thickness);
-    }
   }
 }
 
